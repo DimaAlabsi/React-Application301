c80451ebf4629a51629d6f2ebb80b4e970e0e1a4
DimaAlabsi <dema.yaser8@gmail.com>
2021-09-07 19:02:25 +0300

    model

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
-
+import Data from './assests/data.json';
+import SelectedBeast from './components/SelectedBeast';
 
 
 // import Header from './Header';
@@ -13,12 +14,42 @@ import Footer from './components/Footer';
 // import Footer from './Footer';
 
  class App extends Component {
+   constructor(props){
+     super(props);
+     this.state={
+       showMd : false,
+       title : "",
+       description :"",
+       image_url: "",
+
+
+     }
+   }
+   handleClose =()=>{
+    this.setState({
+
+      showMd:false
+    })
+  }
+  handleOpen=(title,description,image_url)=>{
+    this.setState({
+
+      showMd: true,
+      title: title,
+      description: description,
+      image_url: image_url,
+
+
+  });
+}
   render() {
     return (
     <>
     <Header/>
-    <Main/>
+    <Main  handleOpen={this.handleOpen}/>
     <Footer/>
+    <SelectedBeast  title={this.state.title} description={this.state.description}
+    image_url={this.state.image_url}  showMd={this.state.showMd} handleClose={this.state.handleClose} />
     </>
       )
   }
